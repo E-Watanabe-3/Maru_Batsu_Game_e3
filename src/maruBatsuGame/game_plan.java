972f3741b274d4b33fba77f96f69a0989aedd94b
DESKTOP-OP0BTK7\渡辺 瑛太 <e.watanabe@vision-sys.co.jp>
2025-05-07 17:10:46 +0900

    no message

---
+++ src/maruBatsuGame/game_plan.java
@@ -0,0 +1,16 @@
+package maruBatsuGame;
+
+//ゲームのルール
+public class game_plan {
+     char[][] board; //盤面3×3の文字配列
+     char player; //プレイヤーネーム
+     char cpu; //コンピューターネーム
+    
+     //コンストラクタ（初期化）
+     public game_plan(char player,char cpu) {
+    	 this.player = player;
+    	 this.cpu = cpu;
+    
+    //盤面作成
+     }
+}
---
+++ src/maruBatsuGame/game_play.java
@@ -0,0 +1,10 @@
+package maruBatsuGame;
+
+public class game_play {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
